4a55250f66489b91b3a8893de7535e5fb0fd65f7
Enrique Ruvalcaba <jordison352@gmail.com>
2023-05-03 12:55:29 -0600

    verify password

--- playground/src/utils/bcrypt.ts
+++ playground/src/utils/bcrypt.ts
@@ -4,4 +4,6 @@ export async function hashPassword(password: string) {
   return await bcrypt.hash(password, 10);
 }
 
-export async function verifyPassword() {}
+export async function verifyPassword(password: string, hash: string) {
+  return await bcrypt.compare(password, hash);
+}
