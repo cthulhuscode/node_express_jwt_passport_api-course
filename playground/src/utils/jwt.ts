8c72e6259938b2bccb38a66a17489722ae4cc65b
Enrique Ruvalcaba <jordison352@gmail.com>
2023-05-08 13:13:21 -0600

    changed name of verifying functions

--- playground/src/utils/jwt.ts
+++ playground/src/utils/jwt.ts
@@ -5,6 +5,10 @@ export function signToken(payload: any) {
   return jwt.sign(payload, config.loginJwtSecret!);
 }
 
-export function verifyToken(token: string) {
+export function verifyLoginToken(token: string) {
   return jwt.verify(token, config.loginJwtSecret!);
+}
+
+export function verifyRecoveryToken(token: string) {
+  return jwt.verify(token, config.recoveryJwtSecret!);
 }
